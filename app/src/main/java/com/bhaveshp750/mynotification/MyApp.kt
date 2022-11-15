717fb4f8d15beff6eeed0a18b4c4a674d7c7b5ed
bhaveshp750 <bhaveshp750@gmail.com>
2022-11-15 17:08:55 +0530

    All Style 8 (Channel Groups)

--- app/src/main/java/com/bhaveshp750/mynotification/MyApp.kt
+++ app/src/main/java/com/bhaveshp750/mynotification/MyApp.kt
@@ -2,6 +2,7 @@ package com.bhaveshp750.mynotification
 
 import android.app.Application
 import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.Color
@@ -18,6 +19,14 @@ const val CHANNEL_1_ID = "channel1"
 const val CHANNEL_1_NAME = "Channel 1"
 const val CHANNEL_2_ID = "channel2"
 const val CHANNEL_2_NAME = "Channel 2"
+
+const val GROUP_1_ID = "group1"
+const val GROUP_2_ID = "group2"
+
+const val CHANNEL_3_ID = "channel3"
+const val CHANNEL_3_NAME = "Channel 3"
+const val CHANNEL_4_ID = "channel4"
+const val CHANNEL_4_NAME = "Channel 4"
 
 class MyApp : Application() {
 
@@ -46,6 +55,18 @@ class MyApp : Application() {
             }
 
             /* - all style - */
+
+            val group1 = NotificationChannelGroup(
+                GROUP_1_ID,
+                "Group 1"
+            )
+
+            val group2 = NotificationChannelGroup(
+                GROUP_2_ID,
+                "Group 2"
+            )
+
+
             val channel1 = NotificationChannel(
                 CHANNEL_1_ID,
                 CHANNEL_1_NAME,
@@ -55,6 +76,8 @@ class MyApp : Application() {
                 enableLights(true)
                 description = "This is Channel 1"
             }
+            channel1.group = GROUP_1_ID
+
             val channel2 = NotificationChannel(
                 CHANNEL_2_ID,
                 CHANNEL_2_NAME,
@@ -64,14 +87,41 @@ class MyApp : Application() {
                 enableLights(true)
                 description = "This is Channel 2"
             }
+            channel2.group = GROUP_1_ID
+
+
+            val channel3 = NotificationChannel(
+                CHANNEL_3_ID,
+                CHANNEL_3_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                lightColor = Color.BLUE
+                enableLights(true)
+                description = "This is Channel 3"
+            }
+            channel3.group = GROUP_2_ID
+
+            val channel4 = NotificationChannel(
+                CHANNEL_4_ID,
+                CHANNEL_4_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                lightColor = Color.YELLOW
+                enableLights(true)
+                description = "This is Channel 4"
+            }
 
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
             manager.createNotificationChannel(counterChannel)
 
             /* - all style - */
+            manager.createNotificationChannelGroup(group1)
+            manager.createNotificationChannelGroup(group2)
             manager.createNotificationChannel(channel1)
             manager.createNotificationChannel(channel2)
+            manager.createNotificationChannel(channel3)
+            manager.createNotificationChannel(channel4)
         }
     }
 }
